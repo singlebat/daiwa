13ab730523e6a5a04e803acf8808546097d86bbd
singlebat <44186558+singlebat@users.noreply.github.com>
2019-03-28 16:11:38 +0900

    Add files via upload

---
+++ 0328/NewLineChangeReport/NewLineChangeReportController.js
@@ -0,0 +1,37 @@
+({
+	doInit : function(component, event, helper) {
+        //コンポーネントをと閉じる
+        var closeAction = function() {
+            var dismissActionPanel = $A.get("e.force:closeQuickAction");
+            dismissActionPanel.fire();
+        }
+        setTimeout(closeAction, 0);
+		var action = component.get("c.getAccount");
+        action.setParams({"LineChangingId": component.get("v.recordId")
+                         });
+        action.setCallback(this, $A.getCallback(function (response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {   
+				var result = response.getReturnValue();
+               	var createRecordEvent = $A.get("e.force:createRecord");
+                createRecordEvent.setParams({
+                    "entityApiName": "LineChangeReport__c",
+                    "defaultFieldValues": {
+                        'LineChange1__c':component.get("v.recordId"),
+                        'Account__c':result,
+                    }
+                });
+                createRecordEvent.fire();	
+            } else if (state === "ERROR") {
+                var toastEvent = $A.get("e.force:showToast");
+                toastEvent.setParams({
+                    "title": "失敗!",
+                    "message": "初期か失敗しました。",
+                    "type":"error"
+                });
+                toastEvent.fire();
+            }
+        }));
+        $A.enqueueAction(action);
+	}
+})
